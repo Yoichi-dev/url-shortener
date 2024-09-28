37874a1016e91f949a12950d9e0c215913d671cf
Yoichi-dev <yoichi.developer@gmail.com>
2024-09-29 01:02:44 +0900

    first commit

---
+++ src/config/log4js.config.ts
@@ -0,0 +1,40 @@
+import appRoot from 'app-root-path';
+import path from 'path';
+
+const LOG_ROOT_DIR = process.env.LOG_ROOT_DIR || appRoot.resolve('logs');
+
+export default {
+  appenders: {
+    console: {
+      type: 'console'
+    },
+    application: {
+      type: 'dateFile',
+      filename: path.join(LOG_ROOT_DIR, './application.log'),
+      pattern: 'yyyyMMdd',
+      keepFileExt: true,
+      numBackups: 7
+    },
+    access: {
+      type: 'dateFile',
+      filename: path.join(LOG_ROOT_DIR, './access.log'),
+      pattern: 'yyyyMMdd',
+      keepFileExt: true,
+      numBackups: 7
+    }
+  },
+  categories: {
+    default: {
+      appenders: ['console'],
+      level: 'ALL'
+    },
+    application: {
+      appenders: ['console', 'application'],
+      level: 'INFO'
+    },
+    access: {
+      appenders: ['console', 'access'],
+      level: 'INFO'
+    }
+  }
+};
